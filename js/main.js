1ad89281cb4305a28840f82dd4e5fda7d886a849
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-11 17:15:15 +0200

    feat: add listaUl const

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 // creo array lista della spesa
 const listaSpesa = ["pasta", "latte", "uova", "zucchero", "sale", "farina", "tonno", "acqua", "birra", "pane", "zucchine"];
 console.log(listaSpesa);
+
+// creo costante ul presa dall'html
+const listaUl = document.getElementById("lista");
+console.log(listaUl);
