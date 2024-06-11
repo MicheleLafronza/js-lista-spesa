c7ce94c16533adf7aaa4218cd51733956c69b968
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-11 17:25:21 +0200

    feat: add while cycle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 // creo array lista della spesa
 const listaSpesa = ["pasta", "latte", "uova", "zucchero", "sale", "farina", "tonno", "acqua", "birra", "pane", "zucchine"];
-console.log(listaSpesa);
+// console.log(listaSpesa);
 
 // creo costante ul presa dall'html
 const listaUl = document.getElementById("lista");
-console.log(listaUl);
+// console.log(listaUl);
+
+// setto la variabile da utilizzare nel ciclo
+let t = 0;
+
+// inizializzo il ciclo per stampare la lista
+while (t < listaSpesa.length) {
+    
+    // creo elemento li
+    let listItem = document.createElement("li");
+
+    // inserisco l'elemento li nel ul
+    listaUl.append(listItem);
+
+    // aggiungo il contenuto (ovvero l'elemento della lista) ad li
+    listItem.innerHTML=(listaSpesa[t]);
+
+    // incremento il valore di t (condizione per non far ripetere il ciclo finita la lista)
+    t++;
+
+}
