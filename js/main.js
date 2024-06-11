ed6743dac118801b9835f20f98a10312f776bbba
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-11 17:12:04 +0200

    feat: add listaSpesa array

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// creo array lista della spesa
+const listaSpesa = ["pasta", "latte", "uova", "zucchero", "sale", "farina", "tonno", "acqua", "birra", "pane", "zucchine"];
+console.log(listaSpesa);
